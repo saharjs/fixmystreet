4e99f415c16c15914be4555eac365c20cee77ec1
Dave Arter <davea@mysociety.org>
2016-06-22 13:35:21 +0100

    Basic white label cobrand
    This removes most of the FMS/mySociety branding, but uses the same logic as the
    fixmystreet.com cobrand wrt problem restrictions/handling/etc.
    
    Rename AGDemo cobrand to Whitelabel

---
+++ perllib/FixMyStreet/Cobrand/Whitelabel.pm
@@ -0,0 +1,20 @@
+package FixMyStreet::Cobrand::Whitelabel;
+use base 'FixMyStreet::Cobrand::FixMyStreet';
+
+sub path_to_web_templates {
+    my $self = shift;
+    return [
+        FixMyStreet->path_to( 'templates/web/whitelabel' ),
+        FixMyStreet->path_to( 'templates/web/fixmystreet.com' ),
+    ];
+}
+sub path_to_email_templates {
+    my ( $self, $lang_code ) = @_;
+    return [
+        FixMyStreet->path_to( 'templates', 'email', 'whitelabel'),
+        FixMyStreet->path_to( 'templates', 'email', 'fixmystreet.com'),
+    ];
+}
+
+
+1;
